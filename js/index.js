63b2785b6a39be38b416eb820f4aa00ed47cd857
Adryann Layon <adryann.layon7@gmail.com>
2022-12-27 21:22:53 -0300

    add: small changes

--- js/index.js
+++ js/index.js
@@ -1,11 +1,19 @@
+import Sound from './sounds.js'
+
 const buttonPlay = document.querySelector('.play')
 const buttonStop = document.querySelector('.stop')
 const buttonPlus = document.querySelector('.plus')
 const buttonLess = document.querySelector('.less')
 const minutesDisplay = document.querySelector('.minutes')
 const secondsDisplay = document.querySelector('.seconds')
+const buttonSoundTree = document.querySelector('.sound-tree')
+const buttonSoundRain = document.querySelector('.sound-rain')
+const buttonSoundCoffeShop = document.querySelector('.sound-coffe-shop')
+const buttonSoundFireplace = document.querySelector('.sound-fireplace')
 let minutes = Number(minutesDisplay.textContent)
 let timeOut
+
+const sound = Sound()
 
 function countdown() {
   timeOut = setTimeout(function () {
@@ -40,9 +48,91 @@ function resetTimer() {
 }
 
 buttonPlay.addEventListener('click', function () {
+  if (minutes <= 0) {
+    minutes = 25
+    minutesDisplay.textContent = minutes
+  }
   countdown()
 })
 
 buttonStop.addEventListener('click', function () {
   resetTimer()
+})
+
+buttonPlus.addEventListener('click', function () {
+  minutes = minutes + 5
+  if (minutes >= 0) {
+    minutesDisplay.textContent = String(minutes).padStart(2, '0')
+  }
+})
+
+buttonLess.addEventListener('click', function () {
+  minutes = minutes - 5
+  if (minutes >= 0) {
+    minutesDisplay.textContent = String(minutes).padStart(2, '0')
+  }
+})
+
+buttonSoundTree.addEventListener('click', function () {
+  if (!buttonSoundTree.classList.contains('active')) {
+    buttonSoundTree.classList.add('active')
+    buttonSoundRain.classList.remove('active')
+    buttonSoundCoffeShop.classList.remove('active')
+    buttonSoundFireplace.classList.remove('active')
+    sound.audioTree()
+    sound.audioRainStop()
+    sound.audioCoffeShopStop()
+    sound.audioFireplaceStop()
+  } else {
+    buttonSoundTree.classList.remove('active')
+    sound.audioTreeStop()
+  }
+})
+
+buttonSoundRain.addEventListener('click', function () {
+  if (!buttonSoundRain.classList.contains('active')) {
+    buttonSoundTree.classList.remove('active')
+    buttonSoundRain.classList.add('active')
+    buttonSoundCoffeShop.classList.remove('active')
+    buttonSoundFireplace.classList.remove('active')
+    sound.audioTreeStop()
+    sound.audioRain()
+    sound.audioCoffeShopStop()
+    sound.audioFireplaceStop()
+  } else {
+    buttonSoundRain.classList.remove('active')
+    sound.audioRainStop()
+  }
+})
+
+buttonSoundCoffeShop.addEventListener('click', function () {
+  if (!buttonSoundCoffeShop.classList.contains('active')) {
+    buttonSoundTree.classList.remove('active')
+    buttonSoundRain.classList.remove('active')
+    buttonSoundCoffeShop.classList.add('active')
+    buttonSoundFireplace.classList.remove('active')
+    sound.audioTreeStop()
+    sound.audioRainStop()
+    sound.audioCoffeShop()
+    sound.audioFireplaceStop()
+  } else {
+    buttonSoundCoffeShop.classList.remove('active')
+    sound.audioCoffeShopStop()
+  }
+})
+
+buttonSoundFireplace.addEventListener('click', function () {
+  if (!buttonSoundFireplace.classList.contains('active')) {
+    buttonSoundTree.classList.remove('active')
+    buttonSoundRain.classList.remove('active')
+    buttonSoundCoffeShop.classList.remove('active')
+    buttonSoundFireplace.classList.add('active')
+    sound.audioTreeStop()
+    sound.audioRainStop()
+    sound.audioCoffeShopStop()
+    sound.audioFireplace()
+  } else {
+    buttonSoundFireplace.classList.remove('active')
+    sound.audioFireplaceStop()
+  }
 })
